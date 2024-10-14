ac1273611709368a901a1fdbbfca152635fe0a1b
Mohmdev <mohammadr_m@outlook.com>
2024-10-14 12:15:38 +0000

    Chore: Update BeforeDashboard component

--- src/components/BeforeDashboard/index.tsx
+++ src/components/BeforeDashboard/index.tsx
@@ -13,24 +13,11 @@ const BeforeDashboard: React.FC = () => {
         <h4>Welcome to your dashboard!</h4>
       </Banner>
       Here&apos;s what to do next:
-      <ul className={`${baseClass}__instructions`}>
-        <li>
-          <SeedButton />
-          {' with a few pages, posts, and projects to jump-start your new site, then '}
-          <a href="/" target="_blank">
-            visit your website
-          </a>
-          {' to see the results.'}
-        </li>
-        <li>
-          If you created this repo using Payload Cloud, head over to GitHub and clone it to your
-          local machine. It will be under the <i>GitHub Scope</i> that you selected when creating
-          this project.
-        </li>
+      <div className={`${baseClass}__instructions`}>
         <li>
           {'Modify your '}
           <a
-            href="https://payloadcms.com/docs/configuration/collections"
+            href="https://payloadcms.com/docs/beta/configuration/collections"
             rel="noopener noreferrer"
             target="_blank"
           >
@@ -38,7 +25,7 @@ const BeforeDashboard: React.FC = () => {
           </a>
           {' and add more '}
           <a
-            href="https://payloadcms.com/docs/fields/overview"
+            href="https://payloadcms.com/docs/beta/fields/overview"
             rel="noopener noreferrer"
             target="_blank"
           >
@@ -46,7 +33,7 @@ const BeforeDashboard: React.FC = () => {
           </a>
           {' as needed. If you are new to Payload, we also recommend you check out the '}
           <a
-            href="https://payloadcms.com/docs/getting-started/what-is-payload"
+            href="https://payloadcms.com/docs/beta/getting-started/what-is-payload"
             rel="noopener noreferrer"
             target="_blank"
           >
@@ -55,18 +42,20 @@ const BeforeDashboard: React.FC = () => {
           {' docs.'}
         </li>
         <li>
-          Commit and push your changes to the repository to trigger a redeployment of your project.
+          {'Visit your website from '}
+          <a href="/" target="_blank">
+            here
+          </a>
+          {'.'}
         </li>
-      </ul>
-      {'Pro Tip: This block is a '}
-      <a
-        href="https://payloadcms.com/docs/admin/components#base-component-overrides"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        custom component
-      </a>
-      , you can remove it at any time by updating your <strong>payload.config</strong>.
+      </div>
+      {'Tip: '}
+      You can{' '}
+      <span>
+        {' '}
+        <SeedButton />{' '}
+      </span>{' '}
+      with a few pages, posts, and projects to jump-start your new site.
     </div>
   )
 }
